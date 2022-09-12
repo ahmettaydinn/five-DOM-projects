8c631e0d26727c5c1706b3219bc11c9e07924040
Ahmet <birceste@gmail.com>
2022-09-12 17:09:22 +0300

    play button fixed

--- Javascript/projectFour.js
+++ Javascript/projectFour.js
@@ -80,4 +80,7 @@ resetBtn.addEventListener("click", function () {
   hours = 0;
 
   document.getElementById("timer").innerHTML = "00:00:00";
+  document.getElementById("startStopBtn").innerHTML =
+    '<i class="fa-solid fa-play" id="play"></i>';
+  timerStatus = "stopped";
 });
